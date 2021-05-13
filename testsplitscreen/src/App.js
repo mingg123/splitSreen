38e210ded949482700314c892d78559013a27ae1
minji_kim2@tmax.co.kr <park7749>
2021-05-13 17:43:10 +0900

    fix px to percent if the size of the screen change it works normally

--- testsplitscreen/src/App.js
+++ testsplitscreen/src/App.js
@@ -8,32 +8,30 @@ const useStyles = makeStyles((theme) => ({
     float: "left",
     width: "33%",
     height: "100px",
-    borderRright: "1px solid #b8b8b8",
+    borderRight: "1px solid #b8b8b8",
     marginRight: "-1px",
-    overflowY: "scroll",
+    overflowY: "none",
   },
   middelScreen : {
     float: "left",
-    width:"33%",
+    width: "33%",
     height: "100px",
-    overflowY:"scroll",
+    overflowY: "none",
   },
   rightScreen : {
     float: "left",
-    width:"33%",
+    width: "33%",
     height: "100px",
-    overflowY:"scroll",
+    overflowY: "none",
   },
   leftborder : {
-    float: "left",
-    width:"0.3%",
+    width:"0.5%",
     height: "100px",
     backgroundColor: "#888888",
     cursor : "pointer"
   },
-  rightborder : {
-    float: "left",
-    width:"0.3%",
+  rightborder : {    
+    width:"0.5%",
     height: "100px",
     backgroundColor: "#888888",
     cursor : "wait"
@@ -44,12 +42,11 @@ function App() {
  let leftStartpos = 0;
  let leftDiffpos= 0;
  let isLeftEnable = false;
- let range = 20;
  let rightStartpos = 0
  let rightDiffpos = 0;
  let isRightEnable = false;
- let Leftpos = 0;
- let Rightpos = 0;
+ let leftpos = 0;
+ let rightpos = 0;
 
  const onLeft_mouse_down = (event) => {
   leftStartpos = event.clientX + leftDiffpos;
@@ -73,31 +70,59 @@ function App() {
  const on_mouse_move = (event) => {
 
   if (isLeftEnable) {
-    Leftpos = event.clientX;
-    leftDiffpos = leftStartpos-Leftpos;
-  
-    var width = (window.innerWidth - document.getElementById("rightWindow").offsetWidth)/2 - 5;
-  
-    if (leftDiffpos > -(width-range) && leftDiffpos < (width-range)) {
-  
-      document.getElementById("leftWindow").style.width = width - leftDiffpos + "px";
+    leftpos = event.clientX;
+    leftDiffpos = leftStartpos-leftpos;
    
-      document.getElementById("middleWindow").style.width = width - 5 + leftDiffpos + "px"; 
-   
-     }
-   }
-   else if (isRightEnable) {
-    Rightpos = event.clientX;
-    rightDiffpos = rightStartpos-Rightpos;
-  
-    var Rightwidth = (window.innerWidth - document.getElementById("leftWindow").offsetWidth)/2 - 5;
+    var totalWidthForLeftAndMiddle = window.innerWidth - document.getElementById("rightWindow").offsetWidth - document.getElementById("hr2").offsetWidth
 
+    let resolutionPercentage = 0.0001;
+    let resolution = window.innerWidth * resolutionPercentage;
+
+    if (leftDiffpos > -(totalWidthForLeftAndMiddle-resolution) && leftDiffpos < (totalWidthForLeftAndMiddle-resolution)) {
+
+      const leftDiffPoint = leftDiffpos / window.innerWidth;
+      // const leftWidthPoint = totalWidthForLeftAndMiddle / resolution / 2 * 0.0001
+      const leftWidthPoint = totalWidthForLeftAndMiddle / window.innerWidth  / 2 ;
+
+      if((leftWidthPoint - leftDiffPoint) < 0.1) { // 왼쪽 퍼센트 제한 
+        return;
+      }
+
+      if((leftWidthPoint + leftDiffPoint) < 0.1) { // 미들 퍼센트 제한 
+        return;
+      }
+
+      document.getElementById("leftWindow").style.width = ((leftWidthPoint - leftDiffPoint)*100) + "%";
+      document.getElementById("middleWindow").style.width = ((leftWidthPoint + leftDiffPoint)*100) + "%"; 
   
-    if (rightDiffpos > -(Rightwidth-range) && rightDiffpos < (Rightwidth-range)) {
-  
-     document.getElementById("middleWindow").style.width = Rightwidth - rightDiffpos + "px";
-  
-     document.getElementById("rightWindow").style.width = Rightwidth - 5 + rightDiffpos + "px"; 
+    }
+  }
+   else if (isRightEnable) {
+
+    rightpos = event.clientX;
+    rightDiffpos = rightStartpos-rightpos;
+
+    var totalWidthForRightAndMiddle = window.innerWidth - document.getElementById("leftWindow").offsetWidth - document.getElementById("hr").offsetWidth
+
+    let resolutionPercentage = 0.0001;
+    let resolution = window.innerWidth * resolutionPercentage;
+
+    if (rightDiffpos > -(totalWidthForRightAndMiddle-resolution) && rightDiffpos < (totalWidthForRightAndMiddle-resolution)) {
+
+      const rightDiffPoint = rightDiffpos / window.innerWidth;
+      const rightWidthPoint = totalWidthForLeftAndMiddle / window.innerWidth  / 2 ;
+
+
+      if((rightWidthPoint - rightDiffPoint) < 0.1) { // 오른쪽 퍼센트 제한 
+        return;
+      }
+
+      if((rightWidthPoint + rightDiffPoint) < 0.1) { // 오른쪽 퍼센트 제한 
+        return;
+      }
+
+      document.getElementById("middleWindow").style.width = ((rightWidthPoint - rightDiffPoint)*100) + "%";
+      document.getElementById("rightWindow").style.width = ((rightWidthPoint + rightDiffPoint)*100) + "%"; 
   
     }
    }
@@ -107,7 +132,7 @@ function App() {
 
 
   return (
-    <div className="Screen" onMouseUp ={on_mouse_up} onMouseMove={on_mouse_move}>
+    <div className="Screen" onMouseUp ={on_mouse_up} onMouseMove={on_mouse_move} style={{ display: "flex", flexDirection: "row" }}>
       <div className={classes.leftScreen} id = "leftWindow">
       LEFT
       </div>
